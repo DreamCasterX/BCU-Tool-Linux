d287c0f1fcd6588a7f5c020e9d83f190d0e5826d
Mike Lu <mcumaple@hotmail.com>
2023-11-16 17:44:02 +0800

    Add emoji and symbolic link for BCU.txt ;  updated Reademe and isolated Get_BCU/Set_BCU/MPM_Lock func

--- README.md
+++ README.md
@@ -1,11 +1,5 @@
 ## A simplified tool works as HP BCU on Linux system
 
-### HOW TO USE:
-#### Copy the two files (.sh and .tgz) to Desktop and type below command on Terminal to run the script:
-```sh
-cd ~/Desktop
-sudo ./MPM_Lock.sh
-```
 
 ## NOTE: 
 ### If make/gcc/g++ tools are not pre-installed (e.g., clean OS), install them before running this script.
@@ -20,4 +14,15 @@ sudo apt install build-essential
 ```sh
 sudo dnf update
 sudo dnf groupinstall "Development Tools"
+```
+
+### HOW TO USE:
+#### Copy the whole 'BCU-Tool-Linux' folder (containing .sh and .tgz files) to HOME directory and run the shell script based on your need::
+```sh
+cd ~/BCU-Tool-Linux
+
+bash Get_BCU_Only.sh   # To get BCU only
+bash Set_BCU_Only.sh   # To set BCU only
+bash MPM_Lock.sh  # To lock MPM
+
 ```
--- Set_BCU_Only.sh
+++ Set_BCU_Only.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
 # CREATOR: Mike Lu
-# CHANGE DATE: 05/08/2023
-
-# HOW TO USE:
-# Copy the two files (.sh and .tgz) to Desktop and type below command on Terminal to run this script:
-# (1) cd ~/Desktop
-# (2) ./Set_BCU_Only.sh
+# CHANGE DATE: 11/16/2023
 
 # NOTE: 
 # If make/gcc/g++ tools are not pre-installed (e.g., clean OS), install them before running this script.
@@ -15,9 +10,14 @@
 # (2) sudo apt install build-essential 
 #
 # On RHEL:
-# (1) sudo dnf makecache
+# (1) sudo dnf update
 # (2) sudo dnf groupinstall "Development Tools"
 
+
+# HOW TO USE:
+# Copy the whole BCU-Tool-Linux folder (containing .sh and .tgz files) to HOME directory and run below command on Terminal:
+# (1) cd ~/BCU-Tool-Linux
+# (2) bash Get_BCU_Only.sh
 
 
 # SET FILE PATH
@@ -28,8 +28,8 @@ BCU=$PWD/sp143035/hpflash-3.22/non-rpms/hp-flash-3.22_x86_64
 
 # EXTRACT HP LINUX TOOLS
 if [ ! -f $SPQ ]; then
-	echo "**ERROR: spxxxxxx.tgz file is not found on the Desktop !!**"
-	read -p 'Press "Ctrl + C" and fix the above error first'	
+	echo "❌ ERROR: spxxxxxx.tgz file is not found!"
+	read -p 'Please fix the above error first and re-try.' && exit 0	
 else
 	tar xf $SPQ
 fi
@@ -38,7 +38,7 @@ fi
 if [ ! -d "/sys/module/hpuefi" ]; then
 	cd $MOD
 	make
-	# sudo rmmod hpuefi   # Uncomment this if you want to remove the old UEFI module
+	# sudo rmmod hpuefi   # Uncomment if you want to remove the old UEFI module
 	sudo insmod hpuefi.ko
 	sudo bash ./mkdevhpuefi
 	sudo make install
@@ -54,16 +54,20 @@ else
 	echo "**HP setup utility is installed**"
 fi
 
-# GET BCU & SET BCU
+
+# SET BCU
 lsmod | grep "hpuefi" 
 if [ $? == 0 ]; then
 	cd $BCU
+
+	# sed -i 's/	Tracking/	Test123/' HPSETUP.TXT 	# Uncomment for Asset Tag testing
+	# sed -i 's/	Test123/	Tracking/' HPSETUP.TXT 	 # Recover to default 
+	# cat HPSETUP.TXT | grep -A 1 "Asset Tracking Number"   # Uncomment for Asset Tag testing
+
 	sudo bash ./hp-repsetup -s -q
-	sudo chown $USER "HPSETUP.TXT"
-	sudo chmod o+w HPSETUP.TXT
-	echo "**BCU is set. Please reboot the system to take effect**"
+	echo "✅ BCU is set. Please reboot the system to take effect."
 	echo ""
 else
-	echo "**ERROR: HP UEFI module is NOT installed successfully !!**"
+	echo "❌ ERROR: HP UEFI module is NOT installed successfully! Please re-try."
 fi
 
