9be12846b4395928b3ceb206c1c79d38ec948449
Mike Lu <mcumaple@hotmail.com>
2023-11-30 15:18:54 +0800

    1. Allow users to rename the root directory (HP-BIOS-Tool-Linux)   2. Check Secure Boot state

--- HpBiosCtl.sh
+++ HpBiosCtl.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# CREATOR: Mike Lu
-# CHANGE DATE: 11/24/2023
+# CREATOR: mike.lu@hp.com
+# CHANGE DATE: 11/30/2023
 
 
 # NOTE: 
@@ -20,11 +20,14 @@
 SPQ=$PWD/sp143035.tgz
 MOD=$PWD/sp143035/hpflash-3.22/non-rpms/hpuefi-mod-3.04
 APP=$PWD/sp143035/hpflash-3.22/non-rpms/hp-flash-3.22_x86_64
-WDIR=/home/$USER/HP-BIOS-Tool-Linux
 
 
 # RESTRICT USER ACCOUNT
 [[ $EUID == 0 ]] && echo -e "⚠️ Please run as non-root user.\n" && exit 0
+
+
+# EXTRACT HP LINUX TOOLS
+[[ ! -f $SPQ ]] && echo -e "❌ ERROR: spxxxxxx.tgz file is not found!\n" && exit 0 || tar xzfm $SPQ
 
 
 # CHECK INTERNET CONNETION
@@ -34,14 +37,11 @@ CheckNetwork() {
 }
 
 
-# EXTRACT HP LINUX TOOLS
-[[ ! -f $SPQ ]] && echo -e "❌ ERROR: spxxxxxx.tgz file is not found!\n" && exit 0 || tar xzf $SPQ
-
-
 # INTALL DEPENDENCIES
 [[ -f /usr/bin/apt ]] && PKG=apt || PKG=dnf
 case $PKG in
    "apt")
+     	[[ ! -f /usr/bin/mokutil ]] && CheckNetwork && sudo apt update && sudo apt install mokutil -y || : 
      	dpkg -l | grep build-essential > /dev/null 
      	[[ $? != 0 ]] && CheckNetwork && sudo apt update && sudo apt install build-essential -y || : 
      	dpkg -l | grep linux-headers-$(uname -r) > /dev/null 
@@ -50,6 +50,7 @@ case $PKG in
      	[[ $? != 0 ]] && CheckNetwork && sudo apt update && sudo apt install fwupd -y || : 
    	;;
    "dnf")
+   	[[ ! -f /usr/bin/mokutil ]] && CheckNetwork && sudo dnf install mokutil -y || :
    	[[ ! -f /usr/bin/make ]] && CheckNetwork && sudo dnf install make -y || :
    	rpm -q kernel-devel-$(uname -r) | grep 'not installed' > /dev/null 
    	[[ $? == 0 ]] && CheckNetwork && sudo dnf install kernel-devel-$(uname -r) -y || :
@@ -59,6 +60,10 @@ case $PKG in
    	[[ $? == 0 ]] && CheckNetwork && sudo dnf install fwupd -y || : 
    	;;
 esac
+
+
+# CHECK SECURE BOOT STATUS
+! mokutil --sb-state | grep 'disabled' > /dev/null && echo -e "⚠️ Secure boot is not disabled!\n" && exit 0
 
 
 # INSTALL UEFI MODULE
@@ -87,22 +92,22 @@ GET_BCU() {
 	cd $APP
 	sudo bash ./hp-repsetup -g -a -q
 	sudo chown $USER HPSETUP.TXT 2> /dev/null
-	sudo chmod o+w HPSETUP.TXT 2> /dev/null && ln -sf $APP/HPSETUP.TXT $WDIR/HPSETUP.TXT 2> /dev/null
-	[[ $? == 0 ]] && echo -e "\n✅ BCU got. Please check HPSETUP.TXT\n" || echo -e "\n❌ ERROR: Failed to get BCU. Please re-run the script.\n"
+	sudo chmod o+w HPSETUP.TXT 2> /dev/null && ln -sf $APP/HPSETUP.TXT $PWD/../../../../HPSETUP.TXT 2> /dev/null
+	[[ $? == 0 ]] && echo -e "\n✅ BCU got. To view, run 'cat HPSETUP.TXT'\n            To edit, run 'open HPSETUP.TXT'\n" || echo -e "\n❌ ERROR: Failed to get BCU. Please re-run the script.\n"
 }
 
 SET_BCU() {
 	cd $APP
-	if [[ -L /$WDIR/HPSETUP.TXT ]]; then 
+	if [[ -L $PWD/../../../../HPSETUP.TXT ]]; then 
 		sudo bash ./hp-repsetup -s -q 
 		echo -e "\n✅ BCU is set. Please reboot the system to take effect.\n" && exit 0
 	fi
-	if [[ ! -L $WDIR/HPSETUP.TXT && -f $WDIR/HPSETUP.TXT ]]; then
-		mv $WDIR/HPSETUP.TXT $APP/HPSETUP.TXT 2> /dev/null && ln -sf $APP/HPSETUP.TXT $WDIR/HPSETUP.TXT 2> /dev/null
+	if [[ ! -L $PWD/../../../../HPSETUP.TXT && -f $PWD/../../../../HPSETUP.TXT ]]; then
+		mv $PWD/../../../../HPSETUP.TXT $APP/HPSETUP.TXT 2> /dev/null && ln -sf $APP/HPSETUP.TXT $PWD/../../../../HPSETUP.TXT 2> /dev/null
 		sudo bash ./hp-repsetup -s -q
 		echo -e "\n✅ BCU is set. Please reboot the system to take effect.\n" && exit 0
 	fi
-	if [[ ! -L $WDIR/HPSETUP.TXT && ! -f $WDIR/HPSETUP.TXT && ! -f $APP/HPSETUP.TXT ]]; then
+	if [[ ! -L $PWD/../../../../HPSETUP.TXT && ! -f $PWD/../../../../HPSETUP.TXT && ! -f $APP/HPSETUP.TXT ]]; then
 		echo -e "❌ ERROR: BCU file is not found!\n" && exit 0
 	else
 		echo -e "\n❌ ERROR: Failed to set BCU. Please re-run the script.\n"
@@ -124,9 +129,9 @@ LOCK_MPM() {
 FLASH_BIOS() {
 	echo -e "\nCurrent system BIOS info: 
 $(sudo dmidecode -t 0 | grep -A1 Version:)\n"
-	! ls $WDIR | grep .cab > /dev/null && echo -e "\n❌ ERROR: BIOS capsule is not found! \n" && exit 0
+	! ls $PWD | grep .cab > /dev/null && echo -e "\n❌ ERROR: BIOS capsule is not found! \n" && exit 0
 	[[ -f /etc/fwupd/daemon.conf ]] && sudo sed -i 's/OnlyTrusted=true/OnlyTrusted=false/' /etc/fwupd/daemon.conf
-	sudo fwupdmgr install $WDIR/*.cab --allow-reinstall --allow-older --force || sudo sed -i 's/OnlyTrusted=true/OnlyTrusted=false/' /etc/fwupd/daemon.conf 2> /dev/null
+	sudo fwupdmgr install $PWD/*.cab --allow-reinstall --allow-older --force || sudo sed -i 's/OnlyTrusted=true/OnlyTrusted=false/' /etc/fwupd/daemon.conf 2> /dev/null
 }
 
 
